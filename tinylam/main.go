019af106e33a790801407594d915f1b9dfee6fac
metaleap <metaleap@users.noreply.github.com>
2019-11-01 14:35:44 +0100

    meandering

--- tinylam/main.go
+++ tinylam/main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strings"
 
 	tl "github.com/metaleap/tinylam/go"
 )
 
 func main() {
-	files, err := ioutil.ReadDir(os.Args[1])
+	files, err := ioutil.ReadDir(".")
 	var prog tl.Prog
 	if err == nil {
 		srcs := make(map[string][]byte, len(files))
 		for _, file := range files {
 			if idxdot := strings.LastIndexByte(file.Name(), '.'); idxdot > 0 && file.Name()[idxdot:] == ".tl" && !file.IsDir() {
-				if srcs[file.Name()[:idxdot]], err = ioutil.ReadFile(filepath.Join(os.Args[1], file.Name())); err != nil {
+				if srcs[file.Name()[:idxdot]], err = ioutil.ReadFile(file.Name()); err != nil {
 					panic(err)
 				}
 			}
 		}
 		prog = tl.Load(srcs)
-		jsonout := json.NewEncoder(os.Stdout)
-		jsonout.SetIndent("", "  ")
-		err = jsonout.Encode(prog)
+		defqname := os.Args[1]
+		if strings.IndexByte(defqname, '.') < 0 {
+			defqname = "appdemo." + defqname + ".main"
+		}
+		if def, ok := prog[defqname]; !ok {
+			panic("unknown: " + defqname)
+		} else {
+			println(def.Str())
+		}
+		println("__________________")
+		println(prog.Run(defqname, make([]byte, 123)).Str())
 	}
 	if err != nil {
 		panic(err)
